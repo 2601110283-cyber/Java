c0c8025e651d0ce465e6f441061aa908054d16d0
2601110284-cyber <2601110283@gspace.kopo.ac.kr>
2026-04-23 10:58:45 +0900

    Java 5장 중첨 if문

---
+++ src/ai0423/BMItest2.java
@@ -0,0 +1,55 @@
+package ai0423;
+
+import java.util.Scanner;
+
+public class BMItest2 {
+    public  static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("===============BMI(BOdy Index Mass)===============");
+
+        System.out.print("* 체중(kg, 실수값) 입력:");
+        double weight = s.nextDouble();
+
+        System.out.print("* 키(cm, 실수값) 입력:");
+        double height = s.nextDouble();
+
+        System.out.print("*성명 입력:");
+        String name = s.nextLine();
+
+        double bmi = weight / Math.pow(height / 100, 2); //((height/100)*(height/100))
+        String result1, result2;
+
+        if(bmi < 18.5) {
+            result1 = "저";
+            result2 = "식이요법과 운동을 통해 체중을 증량시켜야 합니다. 생명에 위험이 있을 수도 있습니다.";
+        }else if(bmi < 22.9){
+            result1 = "정상";
+            result2 = "현재 체중을 유짛시기 바랍니다.";
+
+        }else if(bmi < 24.9) {
+            result1 = "고";
+            result2 = "식단과 운동을 통해 체중을 감량하시기 바랍니다.";
+        }else if(bmi < 29.9) {
+            result1 = "비만";
+            result2 = "식단과 운동을 통해 체중을 감량하시기 바랍니다. 성인병 유발의 원인이 됩니다.";
+        }else{
+            result1 = "고도비만";
+            result2 = "전문가의 도움을 받아 식단과 운동을 통해 체중을 감량하시기 바랍니다. 고혈압, 고지혈증, 당뇨병의 원인입니다.";
+        }
+
+
+
+
+
+
+
+
+        System.out.printf("* %s님의 BMI 지수 결과: %.2f㎏/㎡", name, bmi);
+        System.out.println("* %s체중은 %s");
+
+        s1.close();
+        s.close();
+    }
+        }
+
